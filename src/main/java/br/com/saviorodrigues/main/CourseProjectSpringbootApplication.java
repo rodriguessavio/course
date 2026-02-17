7c1f68349aa641c6bb4cd0e6546be2e60aa022de
rodriguessavio <capar.2022113tads0049@aluno.ifpi.edu.br>
2026-02-17 11:46:51 -0300

    first commit

---
+++ src/main/java/br/com/saviorodrigues/main/CourseProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.saviorodrigues.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseProjectSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/saviorodrigues/main/CourseProjectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saviorodrigues.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
